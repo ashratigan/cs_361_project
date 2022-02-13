6b97193ecbad031d3e7549222ae192ff322210f6
Ashley <ashley.ratigan@gmail.com>
2022-02-13 18:27:14 -0500

    Created intial UI to display climbing converter

---
+++ script.js
@@ -0,0 +1,44 @@
+// code for form on ready page
+document.addEventListener('DOMContentLoaded', submitForm);
+
+function submitForm(){
+    document.getElementById('formSubmit') && document.getElementById('formSubmit').addEventListener('click', function(event){
+        const convertFrom = document.getElementById("convertFrom").value
+        const convertTo = document.getElementById("convertTo").value
+        const grade = document.getElementById("grade").value
+        const payload =  {convertFrom, convertTo, grade}
+
+        console.log(convertFrom, convertTo, payload)
+        if (convertFrom.includes("Select") || convertTo.includes("Select") || grade.includes("Select")) {
+            event.preventDefault();
+            document.getElementById('form-response').textContent = "Oops, you need to select a climbing grade to convert from, a grade, and a climbing grade to convert to!";
+            document.getElementById('form-response').style.color = "red"
+        } else {
+            event.preventDefault();
+            const req = new XMLHttpRequest();
+            // TODO: post to actual endpoint
+            req.open("POST", "https://httpbin.org/post");
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.addEventListener('load',function(){
+            if(req.status >= 200 && req.status < 400){
+                const response = JSON.parse(req.responseText);
+            //   document.getElementById('ready-form-container').textContent = null;
+                document.getElementById('form-response').textContent = "Success!";
+                document.getElementById('form-response').style.color = "green";
+            } else {
+                console.log("Error in network request: " + req.statusText);
+                document.getElementById('form-response').textContent = "Oops, something went wrong!";
+                document.getElementById('form-response').style.color = "red"
+            }});
+            req.send(JSON.stringify(payload));
+        } 
+    })
+}
+// code for form on ready page
+document.addEventListener('DOMContentLoaded', clearForm);
+
+function clearForm(){
+    document.getElementById('formClear') && document.getElementById('formClear').addEventListener('click', function(event){
+        document.getElementById('form-response').textContent = ""
+    })
+}
